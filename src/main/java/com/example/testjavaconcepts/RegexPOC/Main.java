77d0418bd19ee2536682af7653a5263644a3b9d8
Esteban <esteban_89_1@hotmail.com>
2019-04-28 22:31:21 +0200

    Regex

---
+++ src/main/java/com/example/testjavaconcepts/RegexPOC/Main.java
@@ -0,0 +1,34 @@
+package com.example.testjavaconcepts.RegexPOC;
+
+// Alternativa 1: Scanner(Source source)    -> Esta manera tambien podrias tomar como input un archivo (file)
+    // useDelimiter(String regex)
+    // useDelimiter(Pattern pattern)
+
+// Alternativa 2: String
+    // split(String regex)
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ScannerPOC();
+        StringPOC();
+    }
+
+    public static void ScannerPOC(){
+        Scanner scanner = new Scanner("Manzana/Pera/Banana").useDelimiter("/");
+        while(scanner.hasNext()){
+            String token = scanner.next();
+            System.out.println(token);
+        }
+        scanner.close();
+    }
+
+    public static void StringPOC(){
+        String input = ("Kiwi/Naranja/Anana");
+        String[] tokens = input.split("/");
+        for(String token: tokens){
+            System.out.println(token);
+        }
+    }
+}
