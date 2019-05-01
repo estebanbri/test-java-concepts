7071b797d5bb0d4b0cc3edd8b7ce832c06fdcd92
Esteban <esteban_89_1@hotmail.com>
2019-05-01 15:33:26 +0200

    String regex vs Pattern regex

--- src/main/java/com/example/testjavaconcepts/PatternPOC/Main.java
+++ src/main/java/com/example/testjavaconcepts/PatternPOC/Main.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ *  -----------------------------------
+ *  String: split() vs Pattern: split()
+ *  -----------------------------------
+ *  El problema de usar el los metodos split(regex) y matches(regex) es que cada vez que les hacer call you recompile the pattern, which cost some resources.
+ *  It is better to compile your pattern once usando la el metodo compile() de la clase Pattern, then try to match it against all the Strings you want.
+ *  Nota:
+ *   String split() internamente calls Pattern.split(regex,  str).
+ *   String matches() internamente calls Pattern.matches(regex,  str).
+ */
+
 public class Main {
     public static void main(String[] args) {
         ScannerPatternPOC();
