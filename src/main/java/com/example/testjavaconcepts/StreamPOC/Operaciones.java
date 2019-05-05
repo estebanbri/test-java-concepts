4c178832ead48cd73730e9f7192ca0edc6111a97
Esteban <esteban_89_1@hotmail.com>
2019-05-05 12:19:43 +0200

    refactor

--- src/main/java/com/example/testjavaconcepts/StreamPOC/Operaciones.java
+++ src/main/java/com/example/testjavaconcepts/StreamPOC/Operaciones.java
@@ -21,168 +21,199 @@ public class Operaciones {
 
     }
 
-    /**
+     /**************************
      *  Operaciones intermedias: IMPORTANTE-> Son LAZY (se ejecutaran si y solo si es alcanzada una operacion terminal)
+     ***************************
      */
 
-    // map()
-    public static Stream<String> mapPOC(List<Persona> list){
-        Stream<Persona> stream1 = list.stream();
-        Stream<String> stream2 = stream1.map(persona -> persona.getNombre());
-        return stream2;
-    }
+        // map()
+        public static Stream<String> mapPOC(List<Persona> list){
+            Stream<Persona> stream1 = list.stream();
+            Stream<String> stream2 = stream1.map(persona -> persona.getNombre());
+            return stream2;
+        }
 
-    // filter()
-    public static Stream<Persona> filterPOC(List<Persona> list){
-        Stream<Persona> stream1 = list.stream();
-        Stream<Persona> stream2 = stream1.filter(persona-> "Esteban".equals(persona.getNombre()));
-        return stream2;
-    }
+        // filter()
+        public static Stream<Persona> filterPOC(List<Persona> list){
+            Stream<Persona> stream1 = list.stream();
+            Stream<Persona> stream2 = stream1.filter(persona-> "Esteban".equals(persona.getNombre()));
+            return stream2;
+        }
 
-    //flatMap()
-    public static Stream<Persona> flatMapPOC(List<List<Persona>> listofList){
-        Stream<List<Persona>> stream1 = listofList.stream();
-        Stream<Persona> stream2 = stream1.flatMap((list)-> list.stream()); // Collecion::stream
-        return stream2;
-    }
+        //flatMap()
+        public static Stream<Persona> flatMapPOC(List<List<Persona>> listofList){
+            Stream<List<Persona>> stream1 = listofList.stream();
+            Stream<Persona> stream2 = stream1.flatMap((list)-> list.stream()); // Collecion::stream
+            return stream2;
+        }
 
-    //distinct()
-    public static Stream<Persona> distinctPOC(List<Persona> list){
-        Stream<Persona> stream1 = list.stream();
-        Stream<Persona> stream2 = stream1.distinct();
-        return stream2;
-    }
+        //distinct()
+        public static Stream<Persona> distinctPOC(List<Persona> list){
+            Stream<Persona> stream1 = list.stream();
+            Stream<Persona> stream2 = stream1.distinct();
+            return stream2;
+        }
 
-    //sorted()
-    public static Stream<Persona> sortedPOC(List<Persona> list){
-        Stream<Persona> stream1 = list.stream();
-        Stream<Persona> stream2 = stream1.sorted(Comparator.comparing(Persona::getNombre));
-        return stream2;
-    }
+        //sorted()
+        public static Stream<Persona> sortedPOC(List<Persona> list){
+            Stream<Persona> stream1 = list.stream();
+            Stream<Persona> stream2 = stream1.sorted(Comparator.comparing(Persona::getNombre));
+            return stream2;
+        }
 
-    //sorted()
-    public static Stream<Integer> sortedPOC(){
-        Stream<Integer> stream1 = Stream.of(6,4,2);
-        Stream<Integer> stream2 = stream1.sorted();
-        return stream2;
-    }
+        //sorted()
+        public static Stream<Integer> sortedPOC(){
+            Stream<Integer> stream1 = Stream.of(6,4,2);
+            Stream<Integer> stream2 = stream1.sorted();
+            return stream2;
+        }
 
-    //peek()
-    public static Stream<Persona> peekPOC(List<Persona> list){
-        Stream<Persona> stream1 = list.stream();
-        Consumer<Persona> consumer = x -> System.out.println(x);
-        Stream<Persona> stream2 = stream1.peek(consumer); // System.out::println
-        return stream2;
-    }
+        //peek()
+        public static Stream<Persona> peekPOC(List<Persona> list){
+            Stream<Persona> stream1 = list.stream();
+            Consumer<Persona> consumer = x -> System.out.println(x);
+            Stream<Persona> stream2 = stream1.peek(consumer); // System.out::println
+            return stream2;
+        }
 
-    //limit()
-    public static Stream<String> limitPOC(){
-        Stream<String> stream1 = Stream.of("A","B","C");
-        Stream<String> stream2 = stream1.limit(2);
-        return stream2;
-    }
+        //limit()
+        public static Stream<String> limitPOC(){
+            Stream<String> stream1 = Stream.of("A","B","C");
+            Stream<String> stream2 = stream1.limit(2);
+            return stream2;
+        }
 
-    //skip()
-    public static Stream<String> skipPOC(){
-        Stream<String> stream1 = Stream.of("A","B","C");
-        Stream<String> stream2 = stream1.skip(2);
-        return stream2;
-    }
+        //skip()
+        public static Stream<String> skipPOC(){
+            Stream<String> stream1 = Stream.of("A","B","C");
+            Stream<String> stream2 = stream1.skip(2);
+            return stream2;
+        }
 
-    /**
-     *  Operaciones terminales
-     */
+     /*************************
+     *  Operaciones terminales:
+     **************************
+     /
 
-    //forEach()
-    public static void forEachPOC(){
-        Stream<String> stream1 = Stream.of("A","B","C");
-        Consumer<String> consumer = x -> System.out.println(x);
-        stream1.forEach(consumer);
-    }
+        /**
+         *
+         * retornan: void
+         */
 
-    //forEachOrdered()
-    public static void forEachOrderedPOC(){
-        Stream<String> stream1 = Stream.of("C","D","A");
-        Consumer<String> consumer = x -> System.out.println(x);
-        stream1.forEachOrdered(consumer);
-    }
+        // forEach | void forEach(Consumer<? super T> action);
+        public static void forEachPOC(){
+            Stream<String> stream1 = Stream.of("A","B","C");
+            Consumer<String> consumer = x -> System.out.println(x);
+            stream1.forEach(consumer);
+        }
 
-    //toArray()
-    public static Object[] toArrayPOC(){
-        Stream<Integer> stream1 = Stream.of(22,11,44);
-        Object[] arr = stream1.toArray();
-        return arr;
-    }
+        //forEachOrdered() | void forEachOrdered(Consumer<? super T> action);
+        public static void forEachOrderedPOC(){
+            Stream<String> stream1 = Stream.of("C","D","A");
+            Consumer<String> consumer = x -> System.out.println(x);
+            stream1.forEachOrdered(consumer);
+        }
 
-    //reduce()
-    public static Integer reducePOC(){
-        Stream<Integer> stream1 = Stream.of(22,11,44);
-        Optional<Integer> optional = stream1.reduce((op1, op2)-> op1 + op2);
-        return optional.get();
-    }
+        /**
+         *
+         * retorna: Object[]
+         */
 
-    //collect()
-    public static List<Integer> collectPOC(){
-        Stream<Integer> stream1 = Stream.of(22,11,44);
-        List<Integer> list = stream1.collect(Collectors.toList());
-        return list;
-    }
+        //toArray() | Object[] toArray();
+        public static Object[] toArrayPOC(){
+            Stream<Integer> stream1 = Stream.of(22,11,44);
+            Object[] arr = stream1.toArray();
+            return arr;
+        }
 
-    //min()
-    public static Integer minPOC(){
-        Stream<Integer> stream1 = Stream.of(22,11,44);
-        Optional<Integer> max = stream1.min(Comparator.naturalOrder());
-        return max.get();
-    }
+        /**
+         *  retorna: List<T>
+         */
+        //collect() | <R, A> R collect(Collector<? super T, A, R> collector);
+        public static List<Integer> collectPOC(){
+            Stream<Integer> stream1 = Stream.of(22,11,44);
+            List<Integer> list = stream1.collect(Collectors.toList());
+            return list;
+        }
 
-    //max()
-    public static Integer maxPOC(){
-        Stream<Integer> stream1 = Stream.of(22,11,44);
-        Optional<Integer> max = stream1.max(Comparator.naturalOrder());
-        return max.get();
-    }
+        /**
+         *
+         * retornan: Optional<T>
+         */
 
-    //count()
-    public static long countPOC(){
-        Stream<String> stream1 = Stream.of("C","D","A");
-        long cantidad = stream1.count();
-        return cantidad;
-    }
+        //reduce() | Optional<T> reduce(BinaryOperator<T> accumulator);
+        public static Optional<Integer> reducePOC(){
+            Stream<Integer> stream1 = Stream.of(22,11,44);
+            Optional<Integer> optional = stream1.reduce((op1, op2)-> op1 + op2);
+            return optional;
+        }
 
-    //anyMatch()
-    public static boolean anyMatchPOC(){
-        Stream<String> stream1 = Stream.of("C","D","A");
-        boolean encontrado = stream1.anyMatch(x->"C".equals(x));
-        return encontrado;
-    }
+        //min() | Optional<T> min(Comparator<? super T> comparator);
+        public static Optional<Integer> minPOC(){
+            Stream<Integer> stream1 = Stream.of(22,11,44);
+            Optional<Integer> max = stream1.min(Comparator.naturalOrder());
+            return max;
+        }
 
-    //allMatch()
-    public static boolean allMatchPOC(){
-        Stream<String> stream1 = Stream.of("C","C","C");
-        boolean encontrado = stream1.anyMatch(x->"C".equals(x));
-        return encontrado;
-    }
+        //max() | Optional<T> max(Comparator<? super T> comparator);
+        public static Integer maxPOC(){
+            Stream<Integer> stream1 = Stream.of(22,11,44);
+            Optional<Integer> max = stream1.max(Comparator.naturalOrder());
+            return max.get();
+        }
 
-    //noneMatch()
-    public static boolean noneMatchPOC(){
-        Stream<String> stream1 = Stream.of("A","A","A");
-        boolean encontrado = stream1.noneMatch(x->"C".equals(x));
-        return encontrado;
-    }
+        //findFirst() | Optional<T> findFirst();
+        public static Optional<String> findFirstPOC(){
+            Stream<String> stream1 = Stream.of("C","D","A");
+            Optional<String> primero = stream1.findFirst();
+            return primero;
+        }
 
-    //findFirst()
-    public static String findFirstPOC(){
-        Stream<String> stream1 = Stream.of("C","D","A");
-        Optional<String> primero = stream1.findFirst();
-        return primero.get();
-    }
+        //findAny() | Optional<T> findAny();
+        public static Optional<String> findAnyPOC(){
+            Stream<String> stream1 = Stream.of("C","D","A");
+            Optional<String> primero = stream1.findAny();
+            return primero;
+        }
 
-    //findAny()
-    public static String findAnyPOC(){
-        Stream<String> stream1 = Stream.of("C","D","A");
-        Optional<String> primero = stream1.findAny();
-        return primero.get();
-    }
+        /**
+         *
+         * retorna: long
+         */
+
+        //count() | long count();
+        public static long countPOC(){
+            Stream<String> stream1 = Stream.of("C","D","A");
+            long cantidad = stream1.count();
+            return cantidad;
+        }
+
+        /**
+          * retornan: boolean
+         */
+
+        //anyMatch() | boolean anyMatch(Predicate<? super T> predicate);
+        public static boolean anyMatchPOC(){
+            Stream<String> stream1 = Stream.of("C","D","A");
+            boolean encontrado = stream1.anyMatch(x->"C".equals(x));
+            return encontrado;
+        }
+
+        //allMatch() |  boolean allMatch(Predicate<? super T> predicate);
+        public static boolean allMatchPOC(){
+            Stream<String> stream1 = Stream.of("C","C","C");
+            boolean encontrado = stream1.anyMatch(x->"C".equals(x));
+            return encontrado;
+        }
+
+        //noneMatch() | boolean noneMatch(Predicate<? super T> predicate);
+        public static boolean noneMatchPOC(){
+            Stream<String> stream1 = Stream.of("A","A","A");
+            boolean encontrado = stream1.noneMatch(x->"C".equals(x));
+            return encontrado;
+        }
+
+
 
     public static class Persona{
         private String nombre;
