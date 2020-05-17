ea73e2b13864998ccdea08186e2e2f88550e41d6
Ignacio Esteban Briceño <esteban_89_1@hotmail.com>
2020-05-17 17:06:11 +0200

    Update MapPOC.java

--- src/main/java/com/example/testjavaconcepts/MapPOC/MapPOC.java
+++ src/main/java/com/example/testjavaconcepts/MapPOC/MapPOC.java
@@ -3,6 +3,17 @@ package com.example.testjavaconcepts.MapPOC;
 import java.util.HashMap;
 import java.util.Map;
 
+//  |   putIfAbsent(K key, V value)   VS   computeIfAbsent(K key, Function<T,V> f)   |
+// Sirven para lo mimso 
+// putIfAbsent y computeIfAbsent terminan sirviendo para lo mismo para agregar entries a tu map. Pero tienen una pequeña diferencia 
+// putIfAbsent primero crea el objeto de value especificado y despues chequea si la key existe, en caso de que la key ya exista
+// no lo va a agregar como entry en el map, pero la desventaja es que te creo el objeto anyway en el heap.
+// En cambio computeIfAbsent al aceptar una lamda exp como argumento primero valida si existe la key en el map y no ejecuta la lamda
+// en caso de que ya exista la key, por ende no te crea al pedo el objeto en el heap.
+
+//  |   compute(K key, BiFunction<T, U, R> f)   VS   computeIfPresent(K key, BiFunction<T, U, R> f)   |
+// Sirven para lo mimso, lo unico que computeIfPresent te salva de nullpointer en caso de que no exista la key.
+
 public class MapPOC {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
