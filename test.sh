9f9b228d0701a9499c4005de8219e881e884585d
Esteban <Cqc589383.g2020>
2021-01-22 22:29:49 -0300

    update

--- test.sh
+++ test.sh
@@ -9,20 +9,22 @@ case $code in
 
   0)
     echo "Merge Automatico realizado exitosamente..."
+    echo "Pusheando commit a rama $currentBranch"
     git push
     ;;
 
   1)
     echo "Merge Automatico realizado parcialmente, es necesario resolver conflictos manualmente"
+    read;
     ;;
 
   128)
     echo "Merge Automatico abortado..."
+    read;
     ;;
 
   *)
     echo "Error: codigo de error no contemplado"
+    read;
     ;;
 esac
-
-read;
