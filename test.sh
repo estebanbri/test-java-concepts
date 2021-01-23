f3e2f17db8e9dcceed3900e9bfa6fd3b1afb46aa
Esteban <Cqc589383.g2020>
2021-01-22 22:45:16 -0300

    update

--- test.sh
+++ test.sh
@@ -8,21 +8,21 @@ code=$?
 case $code in
 
   0)
-    echo -e "\e[32mMerge Automatico realizado exitosamente..."
+    echo -e "\e[32mMerge Automatico realizado exitosamente...\e[0m"
     echo "Pusheando commits a rama $currentBranch"
     git push
     ;;
 
   1)
-    echo -e "\e[31mMerge Automatico realizado parcialmente, es necesario resolver conflictos manualmente"
+    echo -e "\e[31mMerge Automatico realizado parcialmente, es necesario resolver conflictos manualmente\e[0m"
     ;;
 
   128)
-    echo -e "\e[31mMerge Automatico abortado..."
+    echo -e "\e[31mMerge Automatico abortado...\e[0m"
     ;;
 
   *)
-    echo -e "\e[31mError: codigo de error no contemplado"
+    echo -e "\e[31mError: codigo de error no contemplado\e[0m"
     ;;
 esac
 read;
